ea402ce833940c7d432e3792dd7ffda9ed38d183
Thibaud Genty <thibaud.genty@cosmian.com>
2022-06-10 11:35:15 +0200

    Remove leftovers

--- crate/server/src/database/mysql.rs
+++ crate/server/src/database/mysql.rs
@@ -586,49 +586,6 @@ impl Database for Sql {
     }
 }
 
-/// Builder of arguments for MySQL crate queries
-/// The MySQL crate supports only passing query args by tuple,
-/// even if by vec compiles fine it doesn't work and results are empty,
-/// so this is the way to build sequentially tuple of dynamic size
-trait TupleAppend<T> {
-    type ResultType;
-    fn append(self, t: T) -> Self::ResultType;
-}
-
-impl<T> TupleAppend<T> for () {
-    type ResultType = (T,);
-
-    fn append(self, t: T) -> Self::ResultType {
-        (t,)
-    }
-}
-
-macro_rules! impl_tuple_append {
-    ( () ) => {};
-    ( ( $t0:ident $(, $types:ident)* ) ) => {
-        impl<$t0, $($types,)* T> TupleAppend<T> for ($t0, $($types,)*) {
-            // Trailing comma, just to be extra sure we are dealing
-            // with a tuple and not a parenthesized type/expr.
-            type ResultType = ($t0, $($types,)* T,);
-
-            fn append(self, t: T) -> Self::ResultType {
-                // Reuse the type identifiers to destructure ourselves:
-                let ($t0, $($types,)*) = self;
-                // Create a new tuple with the original elements, plus the new one:
-                ($t0, $($types,)* t,)
-            }
-        }
-
-        // Recurse for one smaller size:
-        impl_tuple_append! { ($($types),*) }
-    };
-}
-
-impl_tuple_append! {
-    // Supports tuples up to size 10:
-    (_1, _2, _3, _4, _5, _6, _7, _8, _9, _10)
-}
-
 // Run these tests using: `cargo make rust-tests`
 #[cfg(test)]
 mod tests {
@@ -970,7 +927,6 @@ mod tests {
             .await?;
 
         let objects = mysql.find(None, None, owner).await?;
-        println!("--------- OBJ: {objects:#?}");
         assert_eq!(objects.len(), 1);
         let (o_uid, o_state, _) = &objects[0];
         assert_eq!(o_uid, &uid);
