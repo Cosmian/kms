ae2fb54e1c53cf53300c9dbe869551f3779c8d97
Sébastien Lambert <sebastien.lambert@cosmian.com>
2022-06-20 14:56:45 +0200

    :recycle: reduce code redundancy between abe and covercrypt

--- crate/cli/src/actions/cover_crypt/entrypoint.rs
+++ crate/cli/src/actions/cover_crypt/entrypoint.rs
@@ -1,7 +1,7 @@
 use clap::StructOpt;
 use cosmian_kms_client::KmsRestClient;
 
-use super::{
+use crate::actions::abe::{
     decrypt::DecryptAction,
     encrypt::EncryptAction,
     keys::{
@@ -27,15 +27,15 @@ pub enum CoverCryptAction {
 impl CoverCryptAction {
     pub async fn process(&self, client_connector: &KmsRestClient) -> eyre::Result<()> {
         match self {
-            CoverCryptAction::Init(action) => action.run(client_connector).await?,
-            CoverCryptAction::Rotate(action) => action.run(client_connector).await?,
-            CoverCryptAction::New(action) => action.run(client_connector).await?,
+            CoverCryptAction::Init(action) => action.run(client_connector, true).await?,
+            CoverCryptAction::Rotate(action) => action.run(client_connector, true).await?,
+            CoverCryptAction::New(action) => action.run(client_connector, true).await?,
             // For the time being, Revoke an user decryption key is not possible. We dismiss the action in the cli.
             // Uncomment the followings to activate that command.
             CoverCryptAction::Revoke(_) => eyre::bail!("Revokation is not supported yet"), // action.run(client_connector).await?,
-            CoverCryptAction::Destroy(action) => action.run(client_connector).await?,
-            CoverCryptAction::Encrypt(action) => action.run(client_connector).await?,
-            CoverCryptAction::Decrypt(action) => action.run(client_connector).await?,
+            CoverCryptAction::Destroy(action) => action.run(client_connector, true).await?,
+            CoverCryptAction::Encrypt(action) => action.run(client_connector, true).await?,
+            CoverCryptAction::Decrypt(action) => action.run(client_connector, true).await?,
         };
 
         Ok(())
