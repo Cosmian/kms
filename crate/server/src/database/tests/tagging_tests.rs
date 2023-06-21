4350e0d1a790a38a14839a2ba47b5c91536763f9
Bruno Grieder <bruno.grieder@cosmian.com>
2023-06-21 09:13:45 +0200

    fixed sqlcipher

--- crate/server/src/database/tests/tagging_tests.rs
+++ crate/server/src/database/tests/tagging_tests.rs
@@ -15,44 +15,54 @@ use cosmian_kms_utils::{
 use tempfile::tempdir;
 use uuid::Uuid;
 
-use super::{cached_sqlcipher::CachedSqlCipher, sqlite::SqlitePool};
-use crate::{database::Database, kms_bail, log_utils::log_init, result::KResult};
+use crate::{
+    database::{cached_sqlcipher::CachedSqlCipher, sqlite::SqlitePool, Database},
+    kms_bail,
+    log_utils::log_init,
+    result::KResult,
+};
 
 #[actix_rt::test]
-async fn test_insert_tags() -> KResult<()> {
+async fn test_tags_sql_cipher() -> KResult<()> {
     let dir = tempdir()?;
 
-    // SQL Cipher test
-    {
-        let mut cs_rng = CsRng::from_entropy();
-        let db_key = Key::<32>::new(&mut cs_rng);
-        let file_path = dir.path().join("test_sqlite_enc.db");
-        if file_path.exists() {
-            std::fs::remove_file(&file_path).unwrap();
-        }
-        let db = CachedSqlCipher::instantiate(&file_path).await?;
-        let params = ExtraDatabaseParams {
-            group_id: 0,
-            key: db_key.clone(),
-        };
-        insert_tags(db, Some(&params)).await?;
-    }
+    // generate a database key
+    let mut cs_rng = CsRng::from_entropy();
+    let db_key = Key::<32>::new(&mut cs_rng);
 
-    // SQLite test
-    #[cfg_attr(feature = "sqlcipher", ignore)]
-    {
-        let file_path = dir.path().join("test_sqlite.db");
-        if file_path.exists() {
-            std::fs::remove_file(&file_path).unwrap();
-        }
-        let db = SqlitePool::instantiate(&file_path).await?;
-        insert_tags(db, None).await?;
+    // SQLCipher uses a directory
+    let dir_path = dir.path().join("test_sqlite_enc.db");
+    if dir_path.exists() {
+        std::fs::remove_dir_all(&dir_path).unwrap();
     }
+    std::fs::create_dir_all(&dir_path).unwrap();
+
+    let db = CachedSqlCipher::instantiate(&dir_path).await?;
+    let params = ExtraDatabaseParams {
+        group_id: 0,
+        key: db_key.clone(),
+    };
+    tags(db, Some(&params)).await?;
     Ok(())
 }
 
-async fn insert_tags<DB: Database>(db: DB, db_params: Option<&ExtraDatabaseParams>) -> KResult<()> {
-    log_init("info");
+#[actix_rt::test]
+#[cfg_attr(feature = "sqlcipher", ignore)]
+async fn test_tags_sqlite() -> KResult<()> {
+    let dir = tempdir()?;
+
+    let file_path = dir.path().join("test_sqlite.db");
+    if file_path.exists() {
+        std::fs::remove_file(&file_path).unwrap();
+    }
+    let db = SqlitePool::instantiate(&file_path).await?;
+    tags(db, None).await?;
+
+    Ok(())
+}
+
+async fn tags<DB: Database>(db: DB, db_params: Option<&ExtraDatabaseParams>) -> KResult<()> {
+    log_init("debug");
     let mut rng = CsRng::from_entropy();
 
     // create a symmetric key with tags
@@ -129,7 +139,7 @@ async fn insert_tags<DB: Database>(db: DB, db_params: Option<&ExtraDatabaseParam
         .find_from_tags(
             &["tag1".to_owned(), "tag2".to_owned(), "tag3".to_owned()][..],
             None,
-            None,
+            db_params,
         )
         .await?;
     assert_eq!(res.len(), 0);
