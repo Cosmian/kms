3c0292ca163615590f3307ebdfb1355cdd1b8aa6
Manuthor <manu.coste@gmail.com>
2023-07-04 16:01:59 +0200

    ci: cleanup

--- scripts/build_push_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -x
-
-# requires a `docker login ghcr.io -u <USER>` before running
-docker rmi -f "$(docker images | grep "ghcr.io/cosmian/kms")"
-docker build . -f delivery/Dockerfile.standalone -t ghcr.io/cosmian/kms:4.4.2
-docker push ghcr.io/cosmian/kms:4.4.2
