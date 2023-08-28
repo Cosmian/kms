0804e9153222cd9126c3d1acf38d388971f9a81f
ThibsG <thibsg@pm.me>
2023-08-28 15:33:28 +0200

    Some improvements

--- crate/server/src/core/certificate/create_subca_certificate.rs
+++ crate/server/src/core/certificate/create_subca_certificate.rs
@@ -47,7 +47,7 @@ async fn locate_ca_signing_key(
         ..Attributes::default()
     };
     set_tags(&mut search_attributes, [KMS_CA, &format!("CA={ca}")])?;
-    debug!("Search attributes: CA: {}", ca);
+    debug!("Search attributes: CA: {ca}");
 
     let locate_request = Locate {
         attributes: search_attributes,
@@ -69,16 +69,15 @@ async fn _create_ca(
     params: Option<&ExtraDatabaseParams>,
 ) -> KResult<CASigningKey> {
     let mut tags = tags.clone();
+    tags.insert(KMS_CA.to_string());
 
     match profile {
         Profile::Root => {
             debug!("Creating Root CA certificate: {ca}",);
-            tags.insert(KMS_CA.to_string());
             tags.insert(format!("CA={ca}"));
         }
         _ => {
             debug!("Creating SubCA certificate: {subca}",);
-            tags.insert(KMS_CA.to_string());
             tags.insert(format!("CA={subca}"));
             tags.insert(format!("CA_parent={ca}"));
         }
@@ -107,7 +106,7 @@ async fn _create_ca(
             create_response.public_key_unique_identifier,
         ),
         _ => (
-            ca_signing_key.unwrap(),
+            ca_signing_key.expect("expected CA signing key here"),
             subca,
             create_response.public_key_unique_identifier,
         ),
@@ -126,11 +125,11 @@ async fn _create_ca(
         profile.clone(),
         signing_key.1,
     )
-    .map_err(|e| KmsError::InvalidRequest(format!("Build CA certificate failed: {}", e)))?;
+    .map_err(|e| KmsError::InvalidRequest(format!("Build CA certificate failed: {e}")))?;
 
     let pem = certificate
         .to_pem()
-        .map_err(|e| KmsError::InvalidRequest(format!("Generate PEM failed: {}", e)))?;
+        .map_err(|e| KmsError::InvalidRequest(format!("Generate PEM failed: {e}")))?;
     debug!("new certificate: pem: {pem}");
 
     // Save new certificate in database. Keep also the public key link. This link uses tags instead of a proper KMIP structure since KMIP Certificate structure does not support attribute.
@@ -192,7 +191,7 @@ async fn create_subca(
 
 async fn locate_or_create_ca_signing_key(
     ca: &str,
-    subca: &str,
+    subca: Option<&str>,
     profile: &Profile,
     tags: &HashSet<String>,
     kms: &KMS,
@@ -202,94 +201,62 @@ async fn locate_or_create_ca_signing_key(
     // Find CA signing key
     let signing_keys_uids = locate_ca_signing_key(ca, kms, owner, params).await?;
 
-    let signer = match signing_keys_uids {
-        Some(uids) => match uids.len() {
-            0 => match profile {
-                Profile::Root => create_root_ca(ca, profile, tags, kms, owner, params).await?,
-                _ => {
-                    return Err(KmsError::ConversionError(format!(
-                        "Internal error: Profile root expected here for this CA: {ca}",
-                    )))
-                }
-            },
-            1 => {
-                let signing_key_uid = uids.first().ok_or(KmsError::InvalidRequest(
-                    "Cannot get first signing key uid".to_string(),
-                ))?;
-                debug!(
-                    "Found signing key matching CA name ({ca}) with unique identifier: \
-                     {signing_key_uid}",
-                );
-                let ca_signing_key =
-                    CASigningKey::from_private_key_uid(ca, signing_key_uid, kms, owner, params)
-                        .await?;
-
-                if ca != subca {
-                    let subca_signing_keys_uids =
-                        locate_ca_signing_key(subca, kms, owner, params).await?;
-                    match subca_signing_keys_uids {
-                        Some(subca_uids) => match subca_uids.len() {
-                            0 => {
-                                create_subca(
-                                    ca,
-                                    ca_signing_key,
-                                    subca,
-                                    profile,
-                                    tags,
-                                    kms,
-                                    owner,
-                                    params,
-                                )
-                                .await?
-                            }
-                            1 => {
-                                let signing_key_uid = subca_uids[0].clone(); // cannot fail
-                                debug!(
-                                    "Found signing key UID: {signing_key_uid} matching CA {subca}",
-                                );
-                                CASigningKey::from_private_key_uid(
-                                    subca,
-                                    &signing_key_uid,
-                                    kms,
-                                    owner,
-                                    params,
-                                )
-                                .await?
-                            }
-                            _ => {
-                                return Err(KmsError::ConversionError(format!(
-                                    "Internal error: more than 1 signing key for this SubCA: {ca}",
-                                )))
-                            }
-                        },
-                        None => {
-                            create_subca(
-                                ca,
-                                ca_signing_key,
-                                subca,
-                                profile,
-                                tags,
-                                kms,
-                                owner,
-                                params,
-                            )
-                            .await?
-                        }
-                    } // match end
-                } else {
-                    ca_signing_key
-                }
-            }
-            _ => {
-                return Err(KmsError::ConversionError(format!(
-                    "Internal error: more than 1 signing key for this CA: {ca}",
-                )))
-            }
-        },
-        None => create_root_ca(ca, profile, tags, kms, owner, params).await?,
+    let Some(signing_keys_uids) = signing_keys_uids else {
+        return create_root_ca(ca, profile, tags, kms, owner, params).await
     };
 
-    Ok(signer)
+    match signing_keys_uids.len() {
+        0 if matches!(profile, Profile::Root) => {
+            create_root_ca(ca, profile, tags, kms, owner, params).await
+        }
+        0 => Err(KmsError::ConversionError(format!(
+            "Internal error: Profile root expected here for this CA: {ca}",
+        ))),
+        1 => {
+            let signing_key_uid = signing_keys_uids
+                .first()
+                .expect("first with non-zero length");
+            debug!(
+                "Found signing key matching CA name ({ca}) with unique identifier: \
+                 {signing_key_uid}",
+            );
+
+            let ca_signing_key =
+                CASigningKey::from_private_key_uid(ca, signing_key_uid, kms, owner, params).await?;
+
+            let Some(subca) = subca else {
+                return Ok(ca_signing_key)
+            };
+
+            let Some(subca_signing_keys_uids) =
+                locate_ca_signing_key(subca, kms, owner, params).await?
+            else {
+                return create_subca(ca, ca_signing_key, subca, profile, tags, kms, owner, params)
+                    .await
+            };
+
+            match subca_signing_keys_uids.len() {
+                0 => {
+                    create_subca(ca, ca_signing_key, subca, profile, tags, kms, owner, params).await
+                }
+                1 => {
+                    let signing_key_uid = subca_signing_keys_uids
+                        .first()
+                        .expect("first with non-zero length");
+                    debug!("Found signing key UID: {signing_key_uid} matching CA {subca}",);
+
+                    CASigningKey::from_private_key_uid(subca, signing_key_uid, kms, owner, params)
+                        .await
+                }
+                _ => Err(KmsError::ConversionError(format!(
+                    "Internal error: more than 1 signing key for this SubCA: {ca}",
+                ))),
+            }
+        }
+        _ => Err(KmsError::ConversionError(format!(
+            "Internal error: more than 1 signing key for this CA: {ca}",
+        ))),
+    }
 }
 
 pub(crate) async fn create_ca_chain(
@@ -300,22 +267,26 @@ pub(crate) async fn create_ca_chain(
     params: Option<&ExtraDatabaseParams>,
 ) -> KResult<CASigningKey> {
     // Split all CA and subCAs
-    let cas = ca
-        .split('/')
-        .map(std::string::ToString::to_string)
-        .collect::<Vec<String>>();
-
+    let cas = ca.split('/').collect::<Vec<_>>();
     let mut last_ca_signing_key = CASigningKey::default();
+
+    // in this loop the condition could be removed with the help of `Iterator::advance_by`
+    // see: https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.advance_by
     for (index, current_ca) in cas.iter().enumerate() {
         if index == 0 {
-            debug!("[{index}]: Creating the root CA certificate: CA: {current_ca}");
-            let profile = Profile::Root;
+            debug!("[0]: Creating the root CA certificate: CA: {current_ca}");
             last_ca_signing_key = locate_or_create_ca_signing_key(
-                current_ca, current_ca, &profile, tags, kms, owner, params,
+                current_ca,
+                None,
+                &Profile::Root,
+                tags,
+                kms,
+                owner,
+                params,
             )
             .await?;
         } else {
-            let ca = cas[index - 1].clone();
+            let ca = &cas[index - 1];
             debug!("[{index}]: Creating the subCA certificate: CA: {ca}, subCA: {current_ca}");
             let profile = Profile::SubCA {
                 issuer: Name::from_str(&format!("CN={ca}")).map_err(|e| {
@@ -327,12 +298,19 @@ pub(crate) async fn create_ca_chain(
             };
 
             last_ca_signing_key = locate_or_create_ca_signing_key(
-                &ca, current_ca, &profile, tags, kms, owner, params,
+                ca,
+                Some(current_ca),
+                &profile,
+                tags,
+                kms,
+                owner,
+                params,
             )
             .await?;
         }
     }
 
-    debug!("Return from create_ca_chain: {:?}", last_ca_signing_key);
+    debug!("Return from create_ca_chain: {last_ca_signing_key:?}");
+
     Ok(last_ca_signing_key)
 }
