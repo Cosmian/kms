f5c2b86d4140f9bc4d0552a8e6e5b7859c4ae19a
Bruno Grieder <bruno.grieder@cosmian.com>
2023-10-21 08:53:19 +0200

    initial work on supporting CSRs with openssl

--- crate/server/src/core/operations/certify.rs
+++ crate/server/src/core/operations/certify.rs
@@ -3,7 +3,12 @@ use cosmian_kms_utils::access::ExtraDatabaseParams;
 use tracing::trace;
 
 use crate::{
-    core::{certificate::create_leaf_certificate::create_certificate, KMS},
+    core::{
+        certificate::{
+            create_leaf_certificate::create_certificate, sign_csr::sign_certificate_request,
+        },
+        KMS,
+    },
     error::KmsError,
     kms_bail,
     result::KResult,
@@ -12,30 +17,22 @@ use crate::{
 pub async fn certify(
     kms: &KMS,
     request: Certify,
-    owner: &str,
+    user: &str,
     params: Option<&ExtraDatabaseParams>,
 ) -> KResult<CertifyResponse> {
     trace!("Certify: {}", serde_json::to_string(&request)?);
     if request.protection_storage_masks.is_some() {
         kms_bail!(KmsError::UnsupportedPlaceholder)
     }
-    if request.certificate_request_type.is_some() {
-        kms_bail!(KmsError::InvalidRequest(
-            "Certificate Request Type not supported".to_string()
-        ))
-    }
     if request.certificate_request_value.is_some() {
+        // sign the certificate using a full CSR
+        sign_certificate_request(kms, request, user, params).await
+    } else if request.attributes.is_some() {
+        // using just a few params passed in the request attributes
+        create_certificate(&request, kms, user, params).await
+    } else {
         kms_bail!(KmsError::InvalidRequest(
-            "Certificate Request Value not supported".to_string()
+            "A CSR or parameters in attributes must be provided".to_string()
         ))
     }
-    if request.attributes.is_none() {
-        kms_bail!(KmsError::InvalidRequest(
-            "No attributes provided".to_string()
-        ))
-    }
-
-    trace!("Certify arguments OK");
-
-    create_certificate(&request, kms, owner, params).await
 }
