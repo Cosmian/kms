ee18eb603e61efd77a3619768ada0b5da6cdf15c
Bruno Grieder <bruno.grieder@cosmian.com>
2023-10-30 19:23:57 +0100

    fixed exports

--- crate/kmip/src/openssl/mod.rs
+++ crate/kmip/src/openssl/mod.rs
@@ -1,5 +1,5 @@
 mod private_key;
 mod public_key;
 
-pub use private_key::kmip_private_key_to_openssl;
-pub use public_key::kmip_public_key_to_openssl;
+pub use private_key::{kmip_private_key_to_openssl, openssl_private_key_to_kmip};
+pub use public_key::{kmip_public_key_to_openssl, openssl_public_key_to_kmip};
