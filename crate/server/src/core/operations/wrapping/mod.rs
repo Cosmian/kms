65102d5479663f349b2e2393e34986d94cf118d0
ThibsG <thibsg@pm.me>
2023-11-06 14:51:59 +0100

    Fix rebase

--- crate/server/src/core/operations/wrapping/mod.rs
+++ crate/server/src/core/operations/wrapping/mod.rs
@@ -1,10 +1,12 @@
-use cosmian_kmip::kmip::{kmip_objects::Object, kmip_types::StateEnumeration};
+use cosmian_kmip::kmip::{
+    kmip_objects::Object, kmip_operations::ErrorReason, kmip_types::StateEnumeration,
+};
 use cosmian_kms_utils::access::{ExtraDatabaseParams, ObjectOperationType};
 use tracing::trace;
 pub(crate) use unwrap::unwrap_key;
 pub(crate) use wrap::wrap_key;
 
-use crate::{core::KMS, kms_bail, result::KResult};
+use crate::{core::KMS, error::KmsError, result::KResult};
 
 mod unwrap;
 mod wrap;
@@ -37,20 +39,27 @@ async fn _get_key(
         "get_key: key_uid_or_tags: {key_uid_or_tags:?}, user: {user}, operation_type: \
          {operation_type:?}"
     );
-    let mut objects: Vec<Object> = kms
+    let mut objects = kms
         .db
         .retrieve(key_uid_or_tags, user, operation_type, params)
         .await?
         .into_iter()
         .filter(|(_uid, owm)| owm.state == StateEnumeration::Active)
         .map(|(_uid, own)| own.object)
-        .collect();
+        .collect::<Vec<_>>();
+
     match objects.len() {
-        0 => kms_bail!("unable to fetch the key with uid or tags: {key_uid_or_tags}. No key found"),
+        0 => Err(KmsError::KmipError(
+            ErrorReason::Item_Not_Found,
+            format!("unable to fetch the key with uid or tags: {key_uid_or_tags:}. No key found"),
+        )),
         1 => Ok(objects.remove(0)),
-        _ => kms_bail!(
-            "unable to fetch the key with uid or tags: {key_uid_or_tags}. Too many keys matching \
-             the passed tags"
-        ),
+        _ => Err(KmsError::KmipError(
+            ErrorReason::Item_Not_Found,
+            format!(
+                "unable to fetch the key with uid or tags: {key_uid_or_tags:}. Too many keys \
+                 matching the passed tags"
+            ),
+        )),
     }
 }
