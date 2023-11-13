a580bc72ec2acbb67b14ffe153d5fdda328bf399
Martin Grenouilloux <martin.grenouilloux@cosmian.com>
2023-11-13 16:59:56 +0100

    fips: remove unused import

--- crate/utils/src/crypto/wrap/encrypt_decrypt.rs
+++ crate/utils/src/crypto/wrap/encrypt_decrypt.rs
@@ -262,11 +262,7 @@ mod tests {
         ec::{EcGroup, EcKey},
         nid::Nid,
     };
-    use openssl::{
-        pkey::{PKey, PKeyRef},
-        rand::rand_bytes,
-        rsa::Rsa,
-    };
+    use openssl::{pkey::PKey, rand::rand_bytes, rsa::Rsa};
 
     #[cfg(not(feature = "fips"))]
     use crate::crypto::curve_25519::operation::create_x25519_key_pair;
