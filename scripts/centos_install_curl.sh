01bb4bb29757b6a4cae3e304a6961cd6df20f224
ThibsG <thibsg@pm.me>
2023-11-17 10:12:51 +0100

    Remove unmaintained action-rs and uniformize checkout versions

---
+++ scripts/centos_install_curl.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Tested on CentOS 7 and CentOS 8
+# Check the latest version at https://curl.se/download/
+VERSION=8.4.0
+
+yum update -y
+yum install wget gcc openssl-devel make -y
+wget https://curl.haxx.se/download/curl-${VERSION}.tar.gz
+tar -xzvf curl-${VERSION}.tar.gz 
+rm -f curl-${VERSION}.tar.gz
+pushd curl-${VERSION}
+./configure --prefix=/usr/local --with-ssl
+make
+make install
+ldconfig
+popd
+rm -rf curl-${VERSION}
