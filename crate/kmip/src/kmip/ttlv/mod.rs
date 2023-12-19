77a57a02c13fe7cbbcb62f5a35fe2536b9cbeef8
Martin Grenouilloux <martin.grenouilloux@cosmian.com>
2023-12-19 17:18:20 +0100

    comments: added hunk of comments on bugfix for to_u32_digits()

--- crate/kmip/src/kmip/ttlv/mod.rs
+++ crate/kmip/src/kmip/ttlv/mod.rs
@@ -460,6 +460,16 @@ impl<'de> Deserialize<'de> for TTLV {
 #[must_use]
 pub fn to_u32_digits(big_int: &BigUint) -> Vec<u32> {
     let mut big_int_bytes = big_int.to_bytes_be();
+    // Since the KMS works with big-endian representation of bytearrays, casting
+    // a group of 4 bytes in big-endian u32 representation needs revert iter so
+    // that if you have a chunk [0, 12, 143, 239] you will do
+    // B = 239 + 143*2^8 + 12*2^16 + 0*2^24 which is the correct way to do. On
+    // top of that, if the number of bytes in `big_int` is not a multiple of 4,
+    // it will behave as if there were leading null bytes which is technically
+    // the case.
+    // In this case, using this to convert a BigUint to a Vec<u32> will not lose
+    // leading null bytes information which might be the case when an EC private
+    // key is legally generated with leading null bytes.
     big_int_bytes.reverse();
     big_int_bytes
         .chunks(4)
