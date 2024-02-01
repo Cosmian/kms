5c36df842d1be7a97307121cd56b93ea4776a3c8
Martin Grenouilloux <martin.grenouilloux@cosmian.com>
2024-02-01 16:50:18 +0100

    fix: removed zeroizing copy of an array

--- crate/utils/src/access.rs
+++ crate/utils/src/access.rs
@@ -96,10 +96,11 @@ impl<'de> Deserialize<'de> for ExtraDatabaseParams {
             .try_into()
             .map_err(|_| serde::de::Error::custom("Could not deserialize ExtraDatabaseParams"))?;
         let group_id = u128::from_be_bytes(group_id_bytes);
-        let key_bytes: [u8; 32] = bytes[16..48]
+
+        let key_bytes: &[u8; AES_256_GCM_KEY_LENGTH] = &bytes[16..48]
             .try_into()
             .map_err(|_| serde::de::Error::custom("Could not deserialize ExtraDatabaseParams"))?;
-        let key = Secret::from_protected_bytes(&key_bytes);
+        let key = Secret::<AES_256_GCM_KEY_LENGTH>::from_protected_bytes(key_bytes);
         Ok(ExtraDatabaseParams { group_id, key })
     }
 }
