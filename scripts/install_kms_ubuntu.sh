1a00b7af42eb543bfa89c8155f659cf2a2a291f5
ThibsG <thibsg@pm.me>
2024-03-01 08:25:28 +0100

    Add Nginx setup to enable HTTPS

--- scripts/install_kms_ubuntu.sh
+++ scripts/install_kms_ubuntu.sh
@@ -8,7 +8,7 @@ systemctl stop supervisor
 systemctl disable supervisor
 
 # Update packages and install unzip
-apt-get update && apt-get install -y unzip
+apt-get update && apt-get install -y unzip nginx
 
 # Download KMS zip file
 curl -o kms-ubuntu-22_04.zip https://package.cosmian.com/kms/4.12.0/ubuntu_22_04.zip
@@ -38,6 +38,41 @@ default_username = "admin"
 [http]
 port = 8080
 hostname = "0.0.0.0"
+EOF
+
+# Configure Nginx
+cat > /etc/nginx/sites-enabled/default <<EOF
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        location / {
+                return 301 https://$host$request_uri;
+        }
+}
+
+server {
+        server_name _;
+        listen 443 ssl;
+
+        ssl_certificate /var/lib/cosmian_vm/data/cert.pem;
+        ssl_certificate_key /var/lib/cosmian_vm/data/key.pem;
+
+        location /.well-known/ {
+                root /var/www/html;
+                # Allow CORS calls: see https://support.google.com/a/answer/10743588?hl=en
+                add_header 'Access-Control-Allow-Origin' '*';
+        }
+
+        location / {
+                proxy_pass http://localhost:8080/;
+        }
+}
 EOF
 
 systemctl start ssh
