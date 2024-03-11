7d22d9a8ede34140ecc422a7d3a263140737086f
Emmanuel Coste <emmanuel.coste@cosmian.com>
2024-03-11 09:17:01 +0100

    fix: remove cfg on OpenSSL error variant

--- crate/kmip/src/crypto/wrap/mod.rs
+++ crate/kmip/src/crypto/wrap/mod.rs
@@ -1,10 +1,14 @@
 mod common;
 #[cfg(test)]
 mod tests;
+#[cfg(feature = "openssl")]
 mod unwrap_key;
+#[cfg(feature = "openssl")]
 mod wrap_key;
 
 const WRAPPING_SECRET_LENGTH: usize = 32;
 
+#[cfg(feature = "openssl")]
 pub use unwrap_key::{unwrap_key_block, unwrap_key_bytes};
+#[cfg(feature = "openssl")]
 pub use wrap_key::{wrap_key_block, wrap_key_bytes};
