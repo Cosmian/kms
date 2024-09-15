c03af794a51fb5e9e140b8d2db61fb1e8c8de615
Bruno Grieder <bruno.grieder@cosmian.com>
2024-09-15 11:27:11 +0200

    remove unfortunate typo...

--- scripts/local_ossl_instl.sh
+++ scripts/local_ossl_instl.sh
@@ -61,4 +61,3 @@ rm -rf "${OPENSSL_DIR:?}/bin"
 rm -rf "${OPENSSL_DIR:?}/share"
 echo -e "\nOpenSSL successfully installed at ${OPENSSL_DIR}"
 echo -e "\nIf this program was not sourced, remember to export the absolute path of ${OPENSSL_DIR} as an environment variable."
-X
