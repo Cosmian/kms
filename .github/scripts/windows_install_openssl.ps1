f8c5c69f041694c0a7aea91a36bcdc28bb69159a
Emmanuel Coste <emmanuel.coste@cosmian.com>
2024-09-16 11:20:27 +0200

    fix: artifacts path for windows

--- .github/scripts/windows_install_openssl.ps1
+++
@@ -1,9 +0,0 @@
-# Set up environment for vcpkg
-$env:VCPKG_INSTALLATION_ROOT
-dir $env:VCPKG_INSTALLATION_ROOT
-vcpkg install openssl[fips,weak-ssl-ciphers]
-
-vcpkg integrate install
-$env:VCPKGRS_DYNAMIC = 1
-$env:OPENSSL_DIR = "$env:VCPKG_INSTALLATION_ROOT\packages\openssl_x64-windows"
-
