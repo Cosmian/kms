0e9ec0790316703694a8090f276c7e0684a527ce
Manuthor <manu.coste@gmail.com>
2024-09-23 11:10:10 +0200

    test: backend in parallel

--- .github/scripts/cargo_build.sh
+++ .github/scripts/cargo_build.sh
@@ -48,6 +48,11 @@ if [ -z "$FEATURES" ]; then
   unset FEATURES
 fi
 
+if [ -z "$KMS_TEST_DB" ]; then
+  echo "Info: KMS_TEST_DB is not set. Forcing sqlite"
+  KMS_TEST_DB="sqlite"
+fi
+
 if [ -z "$SKIP_SERVICES_TESTS" ]; then
   echo "Info: SKIP_SERVICES_TESTS is not set."
   unset SKIP_SERVICES_TESTS
@@ -57,7 +62,7 @@ rustup target add "$TARGET"
 
 # Before building the crates, test crates individually on specific features
 cargo install --version 0.6.31 cargo-hack --force
-crates=("crate/kmip" "crate/client" "crate/server")
+crates=("crate/kmip" "crate/client" "crate/cli" "crate/server")
 for crate in "${crates[@]}"; do
   echo "cargo hack on $crate"
   cd "$crate"
@@ -110,26 +115,12 @@ rm -f /tmp/*.json
 
 export RUST_LOG="cosmian_kms_cli=debug,cosmian_kms_server=debug"
 
-# Testing on different databases
-# databases=("redis-findex" "mysql" "sqlite" "postgresql" "sqlite-enc")
-databases=("redis-findex" "sqlite" "postgresql" "sqlite-enc") # for now, do not handle mysql deadlock occurring in CI
-for database in "${databases[@]}"; do
-  # Discard test if skipping options have been passed in `SKIP_SERVICES_TESTS`
-  database_1=$(echo "$database" | cut -d '-' -f 1)
-  database_2=$(echo "$database" | cut -d '-' -f 2)
-  if [ -n "$database_1" ] && [[ "$SKIP_SERVICES_TESTS" == *"$database_1"* ]]; then
-    echo "Skipping tests for $database"
-    continue
-  fi
-  if [ -n "$database_2" ] && [[ "$SKIP_SERVICES_TESTS" == *"$database_2"* ]]; then
-    echo "Skipping tests for $database"
-    continue
-  fi
-  # shellcheck disable=SC2086
-  cargo build --target $TARGET $RELEASE $FEATURES
-  # shellcheck disable=SC2086
-  KMS_TEST_DB="$database" cargo test --target $TARGET $RELEASE $FEATURES --workspace -- --nocapture $SKIP_SERVICES_TESTS
-done
+# shellcheck disable=SC2086
+cargo build --target $TARGET $RELEASE $FEATURES
+
+echo "Database KMS: $KMS_TEST_DB"
+# shellcheck disable=SC2086
+cargo test --target $TARGET $RELEASE $FEATURES --workspace -- --nocapture $SKIP_SERVICES_TESTS
 
 # Uncomment this code to run tests indefinitely
 # counter=1
