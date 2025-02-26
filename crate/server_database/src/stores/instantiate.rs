a031ff98c83a57e1d58dc98ea52d73739227a110
Bruno Grieder <bruno.grieder@cosmian.com>
2025-02-26 09:05:48 +0100

    sql store

--- crate/server_database/src/stores/instantiate.rs
+++ crate/server_database/src/stores/instantiate.rs
@@ -1,54 +1,63 @@
-use std::ops::Deref;
-
-use cosmian_kms_interfaces::{DbState, InterfaceError, InterfaceResult};
+use cosmian_kms_interfaces::{InterfaceError, InterfaceResult, Migrate, ObjectsStore};
 use rawsql::Loader;
-use sqlx::{Acquire, Arguments, Executor, IntoArguments, Pool, Transaction};
+use sqlx::{Executor, IntoArguments, Pool, Transaction};
 
-use crate::{
-    db_error,
-    error::DbResultHelper,
-    stores::sqlite::{atomic_, clear_database_},
-    Database, DbError,
-};
+use crate::{error::DbResultHelper, DbError};
 
-fn get_query(loader: &Loader, name: &str) -> &String {
-    loader
-        .get(name)
-        .ok_or_else(|| db_error!("{} SQL query can't be found", $name))?
-}
-
-pub async fn instantiate<'a, 'c, DB>(loader: &Loader, pool: &Pool<DB>) -> InterfaceResult<()>
+trait SqlStore<'a, DB>: ObjectsStore + Migrate
 where
     DB: sqlx::Database,
     for<'z> &'z mut DB::Connection: Executor<'z, Database = DB>,
-    DB::Arguments<'c>: IntoArguments<'c, DB>,
+    DB::Arguments<'a>: IntoArguments<'a, DB>,
 {
-    let is_new = sqlx::query("SELECT * FROM objects LIMIT 1")
-        .fetch_optional(pool)
-        .await
-        .is_err();
+    fn get_pool(&self) -> &'a Pool<DB>;
 
-    let mut tx = pool
-        .begin()
-        .await
-        .map_err(|e| InterfaceError::Db(format!("failed to start a transaction: {e}")))?;
+    fn get_loader(&self) -> &'a Loader;
 
-    match create_tables(loader, &mut tx).await {
-        Ok(()) => {
-            tx.commit().await.map_err(|e| {
-                InterfaceError::Db(format!("failed to commit the transaction: {e}"))
-            })?;
-            Ok(())
+    async fn instantiate(&self) -> InterfaceResult<()> {
+        let is_new_instance = self.setup_database().await?;
+        if is_new_instance {
+            self.set_current_db_version(env!("CARGO_PKG_VERSION")).await
+        } else {
+            self.migrate().await
         }
-        Err(e) => {
-            tx.rollback().await.context("transaction failed")?;
-            Err(InterfaceError::Db(format!("{e}")))
+    }
+
+    async fn setup_database(&self) -> InterfaceResult<bool> {
+        let is_new_instance = sqlx::query("SELECT * FROM objects LIMIT 1")
+            .fetch_optional(self.get_pool())
+            .await
+            .is_err();
+
+        let mut tx = self
+            .get_pool()
+            .begin()
+            .await
+            .map_err(|e| InterfaceError::Db(format!("failed to start a transaction: {e}")))?;
+
+        match create_tables(self.get_loader(), &mut tx).await {
+            Ok(()) => {
+                tx.commit().await.map_err(|e| {
+                    InterfaceError::Db(format!("failed to commit the transaction: {e}"))
+                })?;
+                Ok(is_new_instance)
+            }
+            Err(e) => {
+                tx.rollback().await.context("transaction failed")?;
+                Err(InterfaceError::Db(format!("{e}")))
+            }
         }
     }
 }
 
-pub async fn create_tables<'a, 'e, DB>(
-    loader: &Loader,
+fn get_query<'a>(loader: &'a Loader, name: &str) -> InterfaceResult<&'a String> {
+    loader
+        .get(name)
+        .ok_or_else(|| InterfaceError::Db(format!("{} SQL query can't be found", name)))
+}
+
+async fn create_tables<'a, 'e, DB>(
+    loader: &'a Loader,
     executor: &mut Transaction<'e, DB>,
 ) -> InterfaceResult<()>
 where
@@ -56,22 +65,22 @@ where
     for<'z> &'z mut DB::Connection: Executor<'z, Database = DB>, // DB::Connection: Deref<Target = E>,
     DB::Arguments<'a>: IntoArguments<'a, DB>,
 {
-    sqlx::query(get_query(loader, "create-table-parameters"))
+    sqlx::query(get_query(loader, "create-table-parameters")?)
         .execute(&mut **executor)
         .await
         .map_err(DbError::from)?;
 
-    sqlx::query(get_query(loader, "create-table-objects"))
+    sqlx::query(get_query(loader, "create-table-objects")?)
         .execute(&mut **executor)
         .await
         .map_err(DbError::from)?;
 
-    sqlx::query(get_query(loader, "create-table-read_access"))
+    sqlx::query(get_query(loader, "create-table-read_access")?)
         .execute(&mut **executor)
         .await
         .map_err(DbError::from)?;
 
-    sqlx::query(get_query(loader, "create-table-tags"))
+    sqlx::query(get_query(loader, "create-table-tags")?)
         .execute(&mut **executor)
         .await
         .map_err(DbError::from)?;
