bc2f50fb2f2a716e9b6fa3dc2b917faaef4ce411
Bruno Grieder <bruno.grieder@cosmian.com>
2025-03-11 10:22:21 +0100

    fips feature needs more testing

--- crate/cli/src/tests/shared/locate.rs
+++ crate/cli/src/tests/shared/locate.rs
@@ -2,6 +2,7 @@ use std::process::Command;
 
 use assert_cmd::prelude::*;
 use cosmian_kms_client::KMS_CLI_CONF_ENV;
+#[cfg(not(feature = "fips"))]
 use cosmian_logger::log_init;
 use kms_test_server::start_default_test_kms_server_with_cert_auth;
 
