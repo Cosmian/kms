495d48202a9356e4a77976bd51bb34b8b6268e6e
phochard <pauline.hochard@cosmian.com>
2025-03-14 12:34:46 +0100

    move code to script file

---
+++ .github/scripts/test_utimaco.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ex
+
+sudo dpkg --add-architecture i386
+sudo apt-get update && sudo apt-get install libc6:i386 libstdc++6:i386
+
+wget "https://package.cosmian.com/ci/hsm-simulator.tar.xz"
+echo -n Extracting compressed archive...
+tar -xvf hsm-simulator.tar.xz
+rm hsm-simulator.tar.xz
+./hsm-simulator/sim5_linux/bin/bl_sim5 -h -o -d ./hsm-simulator/sim5_linux/devices &
+
+sudo cp ./hsm-simulator/libcs_pkcs11_R3.so /lib
+sudo mkdir -p /etc/utimaco
+sudo chmod 755 /etc/utimaco
+sudo cp ./hsm-simulator/cs_pkcs11_R3.cfg /etc/utimaco/
+sudo chmod 766 /etc/utimaco/cs_pkcs11_R3.cfg
+sudo echo -e "...\nLogpath = /tmp\n...\nLogging = 3\n...\nDevice = 3001@localhost \n..." > /etc/utimaco/cs_pkcs11_R3.cfg
+export CS_PKCS11_R3_CFG=/etc/utimaco/cs_pkcs11_R3.cfg
+
+cd ./hsm-simulator/Administration
+./p11tool2 Slot=0 GetSlotInfo
+
+cargo test -p utimaco_pkcs11_loader --features utimaco
