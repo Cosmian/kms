90d6edcbece10cd436167ee2ecf1adeb3267dac9
phochard <pauline.hochard@cosmian.com>
2025-03-18 17:29:50 +0100

    debug login failed

--- .github/scripts/test_utimaco.sh
+++ .github/scripts/test_utimaco.sh
@@ -20,17 +20,16 @@ sudo echo -e "[Global]\nLogpath = /tmp\nLogging = 3\n[CryptoServer]\nDevice = 30
 export CS_PKCS11_R3_CFG=/etc/utimaco/cs_pkcs11_R3.cfg
 
 cd ./hsm-simulator/Administration
-ls -alh
 
 # set the SO PIN to 11223344
-./p11tool2 Slot=0 login=ADMIN,./key/ADMIN_SIM.key  InitToken=11223344
+sudo ./p11tool2 Slot=0 login=ADMIN,./key/ADMIN_SIM.key  InitToken=11223344
 # Change the SO PIN to 12345678
-./p11tool2 Slot=0 LoginSO=11223344 SetPin=11223344,12345678
+sudo ./p11tool2 Slot=0 LoginSO=11223344 SetPin=11223344,12345678
 # Set the User PIN to 11223344
-./p11tool2 Slot=0 LoginSO=12345678 InitPin=11223344
+sudo ./p11tool2 Slot=0 LoginSO=12345678 InitPin=11223344
 # Change the User PIN to 12345678
-./p11tool2 Slot=0 LoginUser=11223344 SetPin=11223344,12345678
+sudo ./p11tool2 Slot=0 LoginUser=11223344 SetPin=11223344,12345678
 
-./p11tool2 Slot=0 GetSlotInfo
+sudo ./p11tool2 Slot=0 GetSlotInfo
 
 HSM_USER_PASSWORD="12345678" cargo test -p utimaco_pkcs11_loader --target x86_64-unknown-linux-gnu --features utimaco -- tests::test_all
