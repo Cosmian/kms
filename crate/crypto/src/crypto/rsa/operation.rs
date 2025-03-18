5bf1b7e2092c4839fe74e05da06b5576775c506c
Bruno Grieder <bruno.grieder@cosmian.com>
2025-03-18 09:57:17 +0100

    imported msks

--- crate/crypto/src/crypto/rsa/operation.rs
+++ crate/crypto/src/crypto/rsa/operation.rs
@@ -303,7 +303,11 @@ pub fn create_rsa_key_pair(
 #[cfg(test)]
 #[allow(clippy::unwrap_used)]
 mod tests {
-    use cosmian_kmip::kmip_2_1::{kmip_attributes::Attributes, kmip_types::CryptographicUsageMask};
+    use cosmian_kmip::kmip_2_1::{
+        extra::fips::{FIPS_MIN_RSA_MODULUS_LENGTH, FIPS_PRIVATE_RSA_MASK, FIPS_PUBLIC_RSA_MASK},
+        kmip_attributes::Attributes,
+        kmip_types::CryptographicUsageMask,
+    };
 
     use crate::crypto::rsa::operation::create_rsa_key_pair;
 
