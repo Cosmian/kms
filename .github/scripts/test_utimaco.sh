bee8fea890f18827df250a1301f21cede60d116c
phochard <pauline.hochard@cosmian.com>
2025-03-19 11:10:22 +0100

    remove cli conf env var

--- .github/scripts/test_utimaco.sh
+++ .github/scripts/test_utimaco.sh
@@ -21,7 +21,6 @@ sudo echo -e "[Global]\nLogpath = /tmp\nLogging = 3\n[CryptoServer]\nDevice = 30
 export CS_PKCS11_R3_CFG=/etc/utimaco/cs_pkcs11_R3.cfg
 
 cd ./hsm-simulator/Administration
-
 # set the SO PIN to 11223344
 ./p11tool2 Slot=0 login=ADMIN,./key/ADMIN_SIM.key  InitToken=11223344
 # Change the SO PIN to 12345678
@@ -30,7 +29,7 @@ cd ./hsm-simulator/Administration
 ./p11tool2 Slot=0 LoginSO=12345678 InitPin=11223344
 # Change the User PIN to 12345678
 ./p11tool2 Slot=0 LoginUser=11223344 SetPin=11223344,12345678
-
 ./p11tool2 Slot=0 GetSlotInfo
 
+cd ../..
 HSM_USER_PASSWORD="12345678" cargo test -p utimaco_pkcs11_loader --target x86_64-unknown-linux-gnu --features utimaco -- tests::test_all
