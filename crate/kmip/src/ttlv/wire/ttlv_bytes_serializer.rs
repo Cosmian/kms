8d127414349a70461a2aa0f814b796b53a35de8f
Bruno Grieder <bruno.grieder@cosmian.com>
2025-03-20 18:23:54 +0100

    fixing ttlv_bytes_deserializer

--- crate/kmip/src/ttlv/wire/ttlv_bytes_serializer.rs
+++ crate/kmip/src/ttlv/wire/ttlv_bytes_serializer.rs
@@ -1,47 +1,6 @@
-use std::{io::Write, str::FromStr};
+use std::io::Write;
 
-use crate::{
-    kmip_1_4, kmip_2_1,
-    ttlv::{error::TtlvError, TTLValue, TtlvType, TTLV},
-};
-
-/// This trait is used to define the KMIP 1.4 and KMIP 2.1 tags that can be used in TTLV serialization
-
-pub trait KmipTag: Copy + ToString + FromStr {
-    /// Get a tag variant from a value
-    fn from_u32(tag_value: u32) -> Result<Self, TtlvError>
-    where
-        Self: Sized;
-
-    /// Convert the tag to an u32 value
-    fn to_u32(&self) -> u32;
-}
-
-impl KmipTag for kmip_1_4::kmip_types::Tag {
-    fn from_u32(tag_value: u32) -> Result<Self, TtlvError> {
-        Self::from_repr(tag_value)
-            .ok_or_else(|| TtlvError::from(format!("Unknown tag value: {tag_value}")))
-    }
-
-    #[allow(clippy::as_conversions)]
-    // This conversion is idomatic for items marked with #[repr(u32)]
-    fn to_u32(&self) -> u32 {
-        *self as u32
-    }
-}
-
-impl KmipTag for kmip_2_1::kmip_types::Tag {
-    fn from_u32(tag_value: u32) -> Result<Self, TtlvError> {
-        Self::from_repr(tag_value)
-            .ok_or_else(|| TtlvError::from(format!("Unknown tag value: {tag_value}")))
-    }
-
-    #[allow(clippy::as_conversions)]
-    // This conversion is idomatic for items marked with #[repr(u32)]
-    fn to_u32(&self) -> u32 {
-        *self as u32
-    }
-}
+use crate::ttlv::{error::TtlvError, wire::kmip_tag::KmipTag, TTLValue, TtlvType, TTLV};
 
 /// Write a tag as a 3-byte big-endian integer
 fn write_tag<W: Write, TAG: KmipTag>(writer: &mut W, tag_str: &str) -> Result<(), TtlvError> {
