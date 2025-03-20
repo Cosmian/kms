6efcb72bd67ec16e51b8607d231d78fda34c9a96
Bruno Grieder <bruno.grieder@cosmian.com>
2025-03-20 18:23:54 +0100

    fixing ttlv_bytes_deserializer

--- crate/kmip/src/ttlv/wire/mod.rs
+++ crate/kmip/src/ttlv/wire/mod.rs
@@ -1,5 +1,6 @@
+mod kmip_tag;
 mod ttlv_bytes_deserializer;
 mod ttlv_bytes_serializer;
 
 pub use ttlv_bytes_deserializer::TTLVBytesDeserializer;
-pub use ttlv_bytes_serializer::{KmipTag, TTLVBytesSerializer};
+pub use ttlv_bytes_serializer::TTLVBytesSerializer;
