19573703d18195d500e48d7fe2aba282152050c6
Bruno Grieder <bruno.grieder@cosmian.com>
2025-04-04 06:05:58 +0200

    query test

--- crate/server/src/tests/ttlv_tests/query.rs
+++ crate/server/src/tests/ttlv_tests/query.rs
@@ -7,10 +7,12 @@ use cosmian_kmip::{
         kmip_types::ProtocolVersion,
     },
     kmip_1_4::{
+        self,
         kmip_messages::RequestMessageBatchItem,
         kmip_operations::{Operation, Query},
         kmip_types::{OperationEnumeration, QueryFunction},
     },
+    kmip_2_1,
     ttlv::KmipFlavor::Kmip1,
 };
 use cosmian_logger::log_init;
@@ -21,7 +23,10 @@ use crate::tests::ttlv_tests::get_client;
 fn test_query() {
     // log_init(option_env!("RUST_LOG"));
     log_init(Some("debug"));
-
+    test_query_(1, 2);
+    test_query_(2, 1);
+}
+fn test_query_(major: i32, minor: i32) {
     let client = get_client();
 
     let query = Query {
@@ -33,8 +38,8 @@ fn test_query() {
     let request_message = RequestMessage {
         request_header: RequestMessageHeader {
             protocol_version: ProtocolVersion {
-                protocol_version_major: 1,
-                protocol_version_minor: 2,
+                protocol_version_major: major,
+                protocol_version_minor: minor,
             },
             batch_count: 1,
             ..Default::default()
@@ -57,22 +62,47 @@ fn test_query() {
     assert_eq!(
         response.response_header.protocol_version,
         ProtocolVersion {
-            protocol_version_major: 1,
-            protocol_version_minor: 2,
+            protocol_version_major: major,
+            protocol_version_minor: minor,
         }
     );
     assert_eq!(response.batch_item.len(), 1);
     let Some(response_batch_item) = response.batch_item.first() else {
         panic!("Expected response batch item");
     };
-    let ResponseMessageBatchItemVersioned::V14(batch_item) = response_batch_item else {
-        panic!("Expected V14 request message");
-    };
-    let Some(Operation::QueryResponse(query_response)) = &batch_item.response_payload else {
-        panic!("Expected QueryResponse");
-    };
-    let Some(operations) = &query_response.operation else {
-        panic!("Expected operations");
-    };
-    assert!(!operations.is_empty());
+    if major == 1 {
+        let ResponseMessageBatchItemVersioned::V14(batch_item) = response_batch_item else {
+            panic!("Expected V14 request message");
+        };
+        let Some(kmip_1_4::kmip_operations::Operation::QueryResponse(query_response)) =
+            &batch_item.response_payload
+        else {
+            panic!("Expected QueryResponse");
+        };
+        let Some(operations) = &query_response.operation else {
+            panic!("Expected operations");
+        };
+        assert!(!operations.is_empty());
+        let Some(object_types) = &query_response.object_type else {
+            panic!("Expected object types");
+        };
+        assert!(!object_types.is_empty());
+    } else {
+        let ResponseMessageBatchItemVersioned::V21(batch_item) = response_batch_item else {
+            panic!("Expected V14 request message");
+        };
+        let Some(kmip_2_1::kmip_operations::Operation::QueryResponse(query_response)) =
+            &batch_item.response_payload
+        else {
+            panic!("Expected QueryResponse");
+        };
+        let Some(operations) = &query_response.operation else {
+            panic!("Expected operations");
+        };
+        assert!(!operations.is_empty());
+        let Some(object_types) = &query_response.object_type else {
+            panic!("Expected object types");
+        };
+        assert!(!object_types.is_empty());
+    }
 }
