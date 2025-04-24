0b2db261fb4a90c33c5d4d026f764286b9e9220f
Bruno Grieder <bruno.grieder@cosmian.com>
2025-04-24 04:21:32 +0200

    more kmip fixes

--- crate/server/src/tests/ttlv_tests/config.rs
+++ crate/server/src/tests/ttlv_tests/config.rs
@@ -1,46 +1,34 @@
+use std::sync::LazyLock;
+
 use cosmian_logger::log_init;
 use openssl::pkcs12::{ParsedPkcs12_2, Pkcs12};
 
 use crate::{
-    socket_server::{create_rustls_server_config, SocketServer, SocketServerParams},
+    socket_server::{SocketServer, SocketServerParams, create_rustls_server_config},
     tests::ttlv_tests::{TEST_HOST, TEST_PORT},
 };
 
 // Static config for tests
-static mut TEST_P12: Option<ParsedPkcs12_2> = None;
-static mut TEST_CLIENT_CA_CERT_PEM: Option<Vec<u8>> = None;
+static TEST_P12: LazyLock<ParsedPkcs12_2> = LazyLock::new(|| {
+    let server_p12_der =
+        include_bytes!("../../../../../test_data/client_server/server/kmserver.acme.com.p12");
+    let server_p12_password = "password";
+    let sealed_p12 =
+        Pkcs12::from_der(server_p12_der).expect("TLS configuration. Failed opening P12");
+    sealed_p12
+        .parse2(server_p12_password)
+        .expect("TLS configuration. Failed to decrypt P12")
+});
+
+static TEST_CLIENT_CA_CERT_PEM: LazyLock<Vec<u8>> =
+    LazyLock::new(|| include_bytes!("../../../../../test_data/client_server/ca/ca.crt").to_vec());
 
 fn load_test_config() -> SocketServerParams<'static> {
-    // Initialize the static data if needed
-    unsafe {
-        if TEST_P12.is_none() {
-            // let server_p12_der = include_bytes!("./certificates/socket_server/server.p12");
-            let server_p12_der = include_bytes!(
-                "../../../../../test_data/client_server/server/kmserver.acme.com.p12"
-            );
-            let server_p12_password = "password";
-
-            // Parse the PKCS#12 object
-            let sealed_p12 =
-                Pkcs12::from_der(server_p12_der).expect("TLS configuration. Failed opening P12");
-            TEST_P12 = Some(
-                sealed_p12
-                    .parse2(server_p12_password)
-                    .expect("TLS configuration. Failed to decrypt P12"),
-            );
-
-            // Store client CA cert
-            TEST_CLIENT_CA_CERT_PEM =
-                Some(include_bytes!("../../../../../test_data/client_server/ca/ca.crt").to_vec());
-            // Some(include_bytes!("./certificates/socket_server/ca.crt").to_vec());
-        }
-
-        SocketServerParams {
-            host: TEST_HOST.to_owned(),
-            port: TEST_PORT,
-            p12: TEST_P12.as_ref().unwrap(),
-            client_ca_cert_pem: TEST_CLIENT_CA_CERT_PEM.as_ref().unwrap(),
-        }
+    SocketServerParams {
+        host: TEST_HOST.to_owned(),
+        port: TEST_PORT,
+        p12: &TEST_P12,
+        client_ca_cert_pem: &TEST_CLIENT_CA_CERT_PEM,
     }
 }
 
