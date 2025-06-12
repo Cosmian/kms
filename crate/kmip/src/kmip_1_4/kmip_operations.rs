31d5f5e24310225851a66052c454bb7092a674d2
Bruno Grieder <bruno.grieder@cosmian.com>
2025-06-12 17:34:47 +0200

    serverinformation to None in 1.x

--- crate/kmip/src/kmip_1_4/kmip_operations.rs
+++ crate/kmip/src/kmip_1_4/kmip_operations.rs
@@ -811,10 +811,12 @@ impl TryFrom<kmip_2_1::kmip_operations::QueryResponse> for QueryResponse {
             operation,
             object_type,
             vendor_identification: value.vendor_identification,
-            server_information: value
-                .server_information
-                .map(TryInto::try_into)
-                .transpose()?,
+            // TODO: what is expected as server_information in KMIP 1 is not clear
+            server_information: None,
+            // server_information: value
+            //     .server_information
+            //     .map(TryInto::try_into)
+            //     .transpose()?,
             extension_information,
             attestation_types: value.attestation_types,
             rng_parameters,
