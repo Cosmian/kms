5ac936dd6544d15f79f1811cf8eaa1d83e6ae0d0
Bruno Grieder <bruno.grieder@cosmian.com>
2025-06-19 17:41:22 +0200

    rebased onto develop

--- crate/server/src/core/operations/activate.rs
+++ crate/server/src/core/operations/activate.rs
@@ -1,11 +1,13 @@
 use std::sync::Arc;
 
-use cosmian_kmip::kmip_2_1::{
-    KmipOperation,
-    kmip_operations::{Activate, ActivateResponse},
-    kmip_types::UniqueIdentifier,
+use cosmian_kms_server_database::reexport::{
+    cosmian_kmip::kmip_2_1::{
+        KmipOperation,
+        kmip_operations::{Activate, ActivateResponse},
+        kmip_types::UniqueIdentifier,
+    },
+    cosmian_kms_interfaces::{ObjectWithMetadata, SessionParams},
 };
-use cosmian_kms_interfaces::{ObjectWithMetadata, SessionParams};
 use tracing::trace;
 
 use crate::{
