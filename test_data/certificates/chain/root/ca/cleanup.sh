1bfb68466d2fb324a30d3c6917d1c423d79dea7a
phochard <pauline.hochard@cosmian.com>
2025-08-04 16:28:24 +0200

    remove test data folder

--- test_data/certificates/chain/root/ca/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -rf certs crl newcerts private intermediate
-rm index.*
-rm serial*
-rm chain_t0.pem
-rm chain_t1.pem
--- test_data/certificates/p12/generate_pkcs12.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-rm output.p12
-
-openssl pkcs12 -export -inkey cert.key -CAfile subca.pem -passout pass:secret -out output.p12 -in cert.pem -certfile subca.pem
