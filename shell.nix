c537fbfc02ce85fd371682215093f1042202a7bf
Manuthor <manu.coste@gmail.com>
2025-10-20 18:21:16 +0200

    fix: nix loading

--- shell.nix
+++ shell.nix
@@ -1,4 +1,7 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/24.05.tar.gz";
+    sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
+  }) {} }:
 
 let
   # Platform flags
