f54723ca416d46e3a69a93a3ad35574c0dc7e1d4
charming_wicket <235307685+charming-wicket-5502@users.noreply.github.com>
2025-12-02 15:51:36 +0100

    Update crate/cli/src/actions/kms/elliptic_curves/signature_verify.rs
    Co-authored-by: Manuthor <32013169+Manuthor@users.noreply.github.com>

--- crate/cli/src/actions/kms/elliptic_curves/signature_verify.rs
+++ crate/cli/src/actions/kms/elliptic_curves/signature_verify.rs
@@ -74,7 +74,7 @@ impl SignatureVerifyAction {
             .with_context(|| "Can't execute the query on the kms server")?;
         let validity_indicator = response
             .validity_indicator
-            .context("Decrypt with RSA: the plaintext is empty")?;
+            .context("Verify with RSA: the signature is empty")?;
 
         let stdout = format!("Signature verification is {validity_indicator}");
         let mut stdout = console::Stdout::new(&stdout);
