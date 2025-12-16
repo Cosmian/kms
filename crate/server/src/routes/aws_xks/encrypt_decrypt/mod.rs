aa0600f14cbc2b57ecb84496e7a49d8013fbe314
Bruno Grieder <bruno.grieder@cosmian.com>
2025-12-16 15:18:08 +0100

    xsk alpha

--- crate/server/src/routes/aws_xks/encrypt_decrypt/mod.rs
+++ crate/server/src/routes/aws_xks/encrypt_decrypt/mod.rs
@@ -1,14 +1,14 @@
-mod decrypt;
-mod encrypt;
-pub use decrypt::{DecryptRequest, DecryptResponse, decrypt};
-pub use encrypt::{EncryptRequest, EncryptResponse, encrypt};
+mod decrypt_;
+mod encrypt_;
+pub(crate) use decrypt_::decrypt;
+pub(crate) use encrypt_::encrypt;
 use serde::{Deserialize, Serialize};
 
 /// Request Payload Parameters: The HTTP body of the request contains the requestMetadata.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[allow(non_snake_case)]
 #[allow(dead_code)]
-pub struct RequestMetadata {
+pub(crate) struct RequestMetadata {
     /// This is the ARN of the principal that invoked KMS Decrypt (see aws:PrincipalArn).
     /// When the caller is another AWS service, this field will contain either
     /// the service principal ending in amazonaws.com, such as ec2.amazonaws.com or
@@ -22,11 +22,11 @@ pub struct RequestMetadata {
     /// a VPC endpoint.
     /// When present, this field indicates the VPC endpoint used for the request (see aws:SourceVpce)
     pub awsSourceVpce: Option<String>,
-    /// This is the ARN of the KMS Key on which the Decrypt, ReDecrypt, GenerateDataKey
-    /// or GenerateDataKeyWithoutPlaintext API was invoked. This field is REQUIRED.
+    /// This is the ARN of the KMS Key on which the Decrypt, `ReDecrypt`, `GenerateDataKey`
+    /// or `GenerateDataKeyWithoutPlaintext` API was invoked. This field is REQUIRED.
     pub kmsKeyArn: String,
     /// This is the KMS API call that resulted in the XKS Proxy API request,
-    /// e.g. CreateKey can result in a GetKeyMetadata call. This field is REQUIRED.
+    /// e.g. `CreateKey` can result in a `GetKeyMetadata` call. This field is REQUIRED.
     /// The XKS Proxy MUST NOT reject a request as invalid if it sees a kmsOperation
     /// other than those listed for this API call.
     /// In the future, KMS may introduce a new API that can be satisfied
@@ -36,9 +36,9 @@ pub struct RequestMetadata {
     /// It is easier for a customer to update their XKS Proxy authorization policy
     /// than to update their XKS Proxy software.
     pub kmsOperation: String,
-    /// This is the requestId of the call made to KMS which is visible in AWS CloudTrail.
+    /// This is the requestId of the call made to KMS which is visible in AWS `CloudTrail`.
     /// The XKS proxy SHOULD log this field to allow a customer
-    /// to correlate AWS CloudTrail entries with log entries in the XKS Proxy.
+    /// to correlate AWS `CloudTrail` entries with log entries in the XKS Proxy.
     /// This field typically follows the format for UUIDs
     /// but the XKS Proxy MUST treat this as an opaque string
     /// and MUST NOT perform any validation on its structure.
@@ -51,13 +51,13 @@ pub struct RequestMetadata {
 
 #[derive(Debug, Serialize, Deserialize)]
 #[allow(non_camel_case_types)]
-pub enum EncrytionAlgorithm {
+pub(crate) enum EncryptionAlgorithm {
     AES_GCM,
 }
 
 /// Ciphertext Data Integrity Value Algorithm
 #[derive(Debug, Serialize, Deserialize)]
 #[allow(non_camel_case_types)]
-pub enum CdivAlgorithm {
+pub(crate) enum CdivAlgorithm {
     SHA_256,
 }
