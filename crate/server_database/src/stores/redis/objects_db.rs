b05d2e53bff869f9136676a01a44f06b4f9a2ddd
HatemMn <19950216+HatemMn@users.noreply.github.com>
2026-01-02 18:26:49 +0100

    fix: method

--- crate/server_database/src/stores/redis/objects_db.rs
+++ crate/server_database/src/stores/redis/objects_db.rs
@@ -182,6 +182,8 @@ impl ObjectsDB {
                 Some(uid.as_bytes()),
             )
             .with_context(|| format!("decrypt_object uid: {uid}"))?;
+        // TODO: load the old version of the cosmian_kmip to support migration from old serialized objects*
+        // then, write using the new one
         let redis_db_object: RedisDbObject = serde_json::from_slice(&plaintext)
             .with_context(|| format!("decrypt_object uid: {uid}"))?;
         Ok(redis_db_object)
