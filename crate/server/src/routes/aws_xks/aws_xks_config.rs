5ab32b7fbbe3fe6627e0420ddfb270c804f150c7
Bruno Grieder <bruno.grieder@cosmian.com>
2026-01-25 10:59:00 +0100

    fix env vars

--- crate/server/src/routes/aws_xks/aws_xks_config.rs
+++ crate/server/src/routes/aws_xks/aws_xks_config.rs
@@ -7,13 +7,13 @@ use serde::{Deserialize, Serialize};
 #[allow(clippy::struct_field_names)]
 pub struct AwsXksConfig {
     /// This setting turns on endpoints handling the AWS XKS feature
-    #[clap(long, env = "KMS_AWX_XKS_ENABLE", default_value = "false")]
+    #[clap(long, env = "KMS_AWS_XKS_ENABLE", default_value = "false")]
     pub aws_xks_enable: bool,
 
     /// The AWS XKS region to use for signing requests (sigv4)
     #[clap(
         long,
-        env = "KMS_AWX_XKS_REGION",
+        env = "KMS_AWS_XKS_REGION",
         required_if_eq("aws_xks_enable", "true")
     )]
     pub aws_xks_region: Option<String>,
@@ -21,14 +21,14 @@ pub struct AwsXksConfig {
     /// The AWS XKS service name to use for signing requests (sigv4)
     #[clap(
         long,
-        env = "KMS_AWX_XKS_SERVICE",
+        env = "KMS_AWS_XKS_SERVICE",
         required_if_eq("aws_xks_enable", "true")
     )]
     pub aws_xks_service: Option<String>,
 
     #[clap(
         long,
-        env = "KMS_AWX_XKS_SIGV4_ACCESS_KEY_ID",
+        env = "KMS_AWS_XKS_SIGV4_ACCESS_KEY_ID",
         required_if_eq("aws_xks_enable", "true")
     )]
     /// The AWS XKS `SigV4` access key ID used to sign requests
@@ -36,14 +36,14 @@ pub struct AwsXksConfig {
 
     #[clap(
         long,
-        env = "KMS_AWX_XKS_SIGV4_SECRET_ACCESS_KEY",
+        env = "KMS_AWS_XKS_SIGV4_SECRET_ACCESS_KEY",
         required_if_eq("aws_xks_enable", "true")
     )]
     pub aws_xks_sigv4_secret_access_key: Option<String>,
 
     #[clap(
         long,
-        env = "KMS_AWX_XKS_KEK_USER",
+        env = "KMS_AWS_XKS_KEK_USER",
         required_if_eq("aws_xks_enable", "true")
     )]
     /// The AWS XKS `SigV4` user that can use the Key Encryption Key (KEK)
