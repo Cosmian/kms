52a6602c445b22b570d9e27ee1866e75ac5c02c3
Manuthor <emmanuel.coste@cosmian.com>
2026-02-15 22:05:06 +0100

    build: release 5.16.1

--- .github/scripts/windows_ui.ps1
+++ .github/scripts/windows_ui.ps1
@@ -57,7 +57,7 @@ function Build-UI {
 {
   "name": "cosmian_kms_client_wasm",
   "type": "module",
-  "version": "5.16.0",
+  "version": "5.16.1",
   "main": "cosmian_kms_client_wasm.js",
   "types": "cosmian_kms_client_wasm.d.ts"
 }
--- documentation/docs/installation/marketplace_guide.md
+++ documentation/docs/installation/marketplace_guide.md
@@ -146,7 +146,7 @@ journalctl -u cosmian_vm_agent
 
 ```console
 $ curl --insecure https://${COSMIAN_VM_IP_ADDR}/version
-"5.16.0"
+"5.16.1"
 ```
 
 !!! info "Why `--allow-insecure-tls` and `--insecure` flags?"
